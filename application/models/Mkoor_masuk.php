9f55201572f07b7a82b5f72be9c14fd240b83c63
Abdurrahman <capteinn@gmail.com>
2018-05-23 05:06:13 +0700

    fix error

--- application/models/Mkoor_masuk.php
+++ application/models/Mkoor_masuk.php
@@ -7,10 +7,9 @@ class Mkoor_masuk extends CI_Model
 
 		$this->db->select('pl.id_pengaduan, p.subjek, p.wkt_pengaduan, k.kategori, r.nama_ruang');
 		$this->db->from('pengaduan_level pl');
+		$this->db->join('pengaduan p','p.id_pengaduan = pl.id_pengaduan');
 		$this->db->join('kategori k','k.id_kategori = p.id_kategori');
 		$this->db->join('ruang r','r.id_ruang = p.id_ruang');
-		$this->db->join('pengaduan_level pl','pl.id_ruang = p.id_ruang');
-		$this->db->where('pengaduan_level pl','pl.id_level = p.id_level');
 		return $this->db->get()->result();
 	}
 }
