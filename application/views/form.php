9179724473493c9e4e210bb78c5b6f9341dcc4e5
isnaini <isnaini2012@gmail.com>
2018-05-23 18:43:26 +0700

    edit form

--- application/views/form.php
+++ application/views/form.php
@@ -1,67 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>form</title>
+  <title>form</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href=<?php echo base_url("assets/logo/vendor/bootstrap/css/bootstrap.min.css")?> rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href=<?php echo base_url("assets/logo/vendor/bootstrap/css/bootstrap.min.css")?> rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href=<?php echo base_url("assets/logo/css/logo-nav.css")?> rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href=<?php echo base_url("assets/logo/css/logo-nav.css")?> rel="stylesheet">
 
-  </head>
+</head>
 
-  <body>
+<body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <img src="http://placehold.it/300x60?text=Logo" width="150" height="30" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!-- Page Content -->
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <h1 class="mt-5">Logo Nav by Start Bootstrap</h1>
+      <a class="navbar-brand" href="#">
+        <img src="http://placehold.it/300x60?text=Logo" width="150" height="30" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-      <div class="card">
+  <!-- Page Content -->
+  <div class="container">
+    <h1 class="mt-5">Sistem Informasi Pengaduan</h1>
+
+    <div class="card" style="background-color: #e5e5e5">
       <div class="card-body">
         <h5 class="card-title"></h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="card-text">
+
+          <form>
+
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label"><b>Password</b></label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+              </div>
+            </div>
+          </form>
+          
+        </div>
+
         <a href="#" class="btn btn-primary right">Go somewhere</a>
       </div>
-</div>
     </div>
-    <!-- /.container -->
+  </div>
+  <!-- /.container -->
 
-    <!-- Bootstrap core JavaScript -->
-    <script src=<?php echo base_url("assets/logo/vendor/jquery/jquery.min.js")?>></script>
-    <script src=<?php echo base_url("assets/logo/vendor/bootstrap/js/bootstrap.bundle.min.js")?>></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src=<?php echo base_url("assets/logo/vendor/jquery/jquery.min.js")?>></script>
+  <script src=<?php echo base_url("assets/logo/vendor/bootstrap/js/bootstrap.bundle.min.js")?>></script>
 
-  </body>
+</body>
 
 </html>
